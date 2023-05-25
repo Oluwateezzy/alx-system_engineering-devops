6a8f0e6a69e72b861f7e37de94e2b15b891f6048
oluwatobiloba <oluwateezzy03@gmail.com>
2023-05-25 06:05:31 -0700

    oop

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,11 +10,8 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    complete = []
-    for t in todo:
-        if t.get("completed") is True:
-            complete.append(t.get("title"))
-    print("Employee {} is done with tasks({}/{})".format(user.get("name"),
-        len(complete), len(todo)))
-    for c in complete:
-        print("\t {}".format(c))
+
+    completed = [t.get("title") for t in todo if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todo)))
+    [print("\t {}".format(c)) for c in completed]
