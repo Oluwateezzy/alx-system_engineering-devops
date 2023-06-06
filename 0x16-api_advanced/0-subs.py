925400aa421d594190adf9d187ee7a72a7e19541
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-06 10:55:16 -0700

    ipdate

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
-"""reddit"""
-import requests
+""" How many subs? """
 
 
 def number_of_subscribers(subreddit):
-    """number of subscriber"""
-    headers = {'User-Agent': 'Custom User Agent'}
+    """ Returns subscriber count of subreddit or 0 """
+    from requests import get
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    r = get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
         return 0
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return 0
+
+    data = js.get("data")
+
+    if data:
+        sub_count = data.get("subscribers")
+        if sub_count:
+            return sub_count
+
+    return 0
