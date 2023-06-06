72d4a8c1b4fabd7581fbbcb0fe9bc2e3235b9864
oluwatobiloba <oluwateezzy03@gmail.com>
2023-06-06 10:28:55 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """number of subscriber"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = { "User-Agent": "Custom User Agent" }
+    headers = {"User-Agent": "Custom User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
